66b0d71806ad9fb92ff06c489e96e95e67723762
sriram507 <isreeram2014@gmail.com>
2025-03-22 00:02:46 +0530

    code updated  for main branch

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+  // Navbar Scroll Effect
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+
+  
